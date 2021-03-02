c0003ef7f316e7a25d6212bee60c8de307a6594b
Facundo Giuliani <facundo.giuliani@gmail.com>
2021-03-02 17:51:13 -0300

    changes

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,14 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-
 export default prisma;
